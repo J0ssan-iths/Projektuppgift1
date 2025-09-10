6d60934b241b8eef34e9b9a5eed85736fe1cc2be
J0ssan-iths <Josefine.Berglund@iths.se>
2025-09-10 15:26:59 +0200

    Ändrat till String metod

---
+++ src/main/java/se/iths/Josefine/Projektuppgift1/HelloWorld.java
@@ -0,0 +1,12 @@
+package se.iths.Josefine.Projektuppgift1;
+
+public class HelloWorld {
+
+    public String HelloWorld(String HelloWorld) {
+        String[] helloWorld = {"Hello", "World", "!"};
+        for(String s: helloWorld) {
+            System.out.println(s);
+        }
+        return "";
+    }
+}
---
+++ src/main/java/se/iths/Josefine/Projektuppgift1/Main.java
@@ -0,0 +1,8 @@
+package se.iths.Josefine.Projektuppgift1;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.HelloWorld(""));
+    }
+}
