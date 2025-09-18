0b8920915ba5c2adb9bccc7d7c1986d6aa774a6d
J0ssan-iths <Josefine.Berglund@iths.se>
2025-09-18 11:12:41 +0200

    Båda uppgifterna med lite ändringar

--- src/main/java/se/iths/Josefine/Projektuppgift1/VG/Main.java
+++ src/main/java/se/iths/Josefine/Projektuppgift1/VG/Main.java
@@ -4,22 +4,19 @@ public class Main {
     public static void main(String[] args) {
         //HelloWorld uppgift
         ArrayExercise helloWorld = new ArrayExercise();
-        System.out.println(helloWorld.helloWorld(""));
+        helloWorld.helloWorld("");
 
         //Uppgift för VG
         //maxArray
-        ArrayExercise array = new ArrayExercise();
-        System.out.println("Det största talet i listan är: " + array.maxArray());
+        System.out.println("Det största talet i listan är: " + helloWorld.maxArray());
         System.out.println();
 
         //Double parameter
-        ArrayExercise double1 = new ArrayExercise();
-        System.out.println("Det största decimaltalet är: " + double1.maxDouble(1.6, 4.8));
+        System.out.println("Det största decimaltalet är: " + helloWorld.maxDouble(1.6, 4.8));
         System.out.println();
 
         //String parametrar
-        ArrayExercise string = new ArrayExercise();
-        System.out.println("Hej! " + string.name("Kalle", "Karlsson"));
+        System.out.println("Hej! " + helloWorld.name("Kalle", "Karlsson"));
 
     }
 }
